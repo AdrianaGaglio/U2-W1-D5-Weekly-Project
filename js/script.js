de43cdaa8fcc3c98fe28873b16858dc92e33e163
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-09-14 17:31:58 +0200

    WIP - sticky sidebar on window scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 window.onscroll = () => {
   const scrollY = window.scrollY; // posizione della Y allo scroll
   const header = document.querySelector("header");
-  if (scrollY > 200) {
+  if (scrollY > 300) {
     header.classList.add("white-bg");
   } else {
     header.classList.remove("white-bg");
+  }
+  if (scrollY >= 850) {
+    const sidebar = document.querySelector("aside div");
+    const top = sidebar.getBoundingClientRect().top;
+    const width = sidebar.getBoundingClientRect().width;
+    const height = sidebar.getBoundingClientRect().height;
+    const right = sidebar.getBoundingClientRect().right - width;
+    const bottom = sidebar.getBoundingClientRect().bottom - height;
+    const left = sidebar.getBoundingClientRect().left;
+    sidebar.style.width = width + "px";
+    sidebar.classList.add("sticky");
   }
 };
